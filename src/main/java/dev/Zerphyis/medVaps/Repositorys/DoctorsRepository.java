3d47561f8c32aa1c1a841a7cc66068c800f23c62
Otavio <estudosprogotavio@gmail.com>
2025-03-12 15:08:55 -0300

    Adicionando repositorio da entidade Medicos

---
+++ src/main/java/dev/Zerphyis/medVaps/Repositorys/DoctorsRepository.java
@@ -0,0 +1,9 @@
+package dev.Zerphyis.medVaps.Repositorys;
+
+import dev.Zerphyis.medVaps.Entity.Doctors.Doctors;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DoctorsRepository extends JpaRepository<Doctors , String>{
+
+}
