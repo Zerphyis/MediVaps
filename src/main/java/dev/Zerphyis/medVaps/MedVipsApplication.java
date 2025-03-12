573f1178056ebf56b9290c53167738d9296f1f72
Otavio <estudosprogotavio@gmail.com>
2025-03-12 15:07:30 -0300

    Commit inicial

---
+++ src/main/java/dev/Zerphyis/medVaps/MedVipsApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.medVaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedVipsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedVipsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Zerphyis/medVaps/MedVipsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.medVaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedVipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
