e606536b2b6c1da06211299cfa79fccba2195233
Otavio <estudosprogotavio@gmail.com>
2025-03-13 03:55:36 -0300

    Adicionando a entidade Consultas

---
+++ src/main/java/dev/Zerphyis/medVaps/Entity/Appointment/Appointment.java
@@ -0,0 +1,86 @@
+package dev.Zerphyis.medVaps.Entity.Appointment;
+
+import dev.Zerphyis.medVaps.Entity.Doctors.Doctors;
+import dev.Zerphyis.medVaps.Entity.Patient.Patient;
+import dev.Zerphyis.medVaps.Entity.Specialty.Specialty;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "consultas")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pacientes_id", nullable = false)
+    private Patient patient;
+
+    @ManyToOne
+    @JoinColumn(name = "medicos_id", nullable = false)
+    private Doctors doctor;
+
+    @Enumerated(EnumType.STRING)
+    private Specialty specialty;
+
+    private LocalDate date;
+    private LocalTime time;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    public Appointment(){
+
+    }
+
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctors getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctors doctor) {
+        this.doctor = doctor;
+    }
+
+    public Specialty getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Specialty specialty) {
+        this.specialty = specialty;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
