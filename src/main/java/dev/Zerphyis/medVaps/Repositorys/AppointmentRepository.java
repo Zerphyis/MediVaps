ed5beb85e895a703968851a00bfb6be9f4948a66
Otavio <estudosprogotavio@gmail.com>
2025-03-13 17:33:00 -0300

    Adicionando a query para buscar entre o ano e data passada

--- src/main/java/dev/Zerphyis/medVaps/Repositorys/AppointmentRepository.java
+++ src/main/java/dev/Zerphyis/medVaps/Repositorys/AppointmentRepository.java
@@ -3,5 +3,9 @@ package dev.Zerphyis.medVaps.Repositorys;
 import dev.Zerphyis.medVaps.Entity.Appointment.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface AppointmentRepository extends JpaRepository<Appointment,Long> {
+    List<Appointment> findByDateBetween(LocalDate startDate, LocalDate endDate);
 }
