fb76881d76f98ea8d7c8afecafabe607263c2925
Otavio <estudosprogotavio@gmail.com>
2025-03-14 04:11:37 -0300

    Adicionando Repostiorio da entidade Login

---
+++ src/main/java/dev/Zerphyis/medVaps/Repositorys/LoginRepository.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.medVaps.Repositorys;
+
+import dev.Zerphyis.medVaps.Entity.Login.Login;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LoginRepository extends JpaRepository<Login,Long> {
+UserDetails findByEmail(String email);
+}
