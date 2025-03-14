1570f8b3a114833f88d33c09d863772d38a9420a
Otavio <estudosprogotavio@gmail.com>
2025-03-14 04:15:45 -0300

    Adicionando filtro de segurança

---
+++ src/main/java/dev/Zerphyis/medVaps/Security/SecurityFilter.java
@@ -0,0 +1,53 @@
+package dev.Zerphyis.medVaps.Security;
+
+import dev.Zerphyis.medVaps.Repositorys.LoginRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    ServiceToken tokenService;
+    @Autowired
+    LoginRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null) {
+            String login = tokenService.validateToken(token);
+            if (!login.isEmpty()) {
+                UserDetails user = repository.findByEmail(login);
+                if (user != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                } else {
+                    System.out.println("Usuário não encontrado: " + login);
+                }
+            } else {
+                System.out.println("Token inválido ou expirado.");
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        String[] parts = authHeader.split(" ");
+        if (parts.length == 2 && "Bearer".equalsIgnoreCase(parts[0])) {
+            return parts[1].trim();
+        }
+        return null;
+    }
+}
