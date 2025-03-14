8de987c4784ec1aa62937d81ca0acff673aeb891
Otavio <estudosprogotavio@gmail.com>
2025-03-14 07:55:20 -0300

    Alterando classe ServiceToken

--- src/main/java/dev/Zerphyis/medVaps/Security/ServiceToken.java
+++ src/main/java/dev/Zerphyis/medVaps/Security/ServiceToken.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import dev.Zerphyis.medVaps.Entity.Login.Login;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,8 @@ public class ServiceToken {
     @Value("${api.security.token.secret}")
     private String secret;
 
-    public String generateToken(Login user){
+
+    public String generateToken(Login user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
@@ -31,20 +33,20 @@ public class ServiceToken {
         }
     }
 
-    public String validateToken(String token){
+    public String validateToken(String token) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            return JWT.require(algorithm)
+            DecodedJWT decodedJWT = JWT.require(algorithm)
                     .withIssuer("medVips")
                     .build()
-                    .verify(token)
-                    .getSubject();
+                    .verify(token);
+            return decodedJWT.getSubject();
         } catch (JWTVerificationException exception) {
-            return "";
+            return null;
         }
     }
 
-    private Instant genExpirationDate(){
+    private Instant genExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
